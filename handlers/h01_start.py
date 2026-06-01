cf2e560232256d81f176be71483fb56e031da3a8
paketkenchi@gmail.com <paketkenchi@gmail.com>
2026-06-01 19:44:09 +0300

    обработка кнопки начать и регистрации

--- handlers/h01_start.py
+++ handlers/h01_start.py
@@ -17,3 +17,21 @@ async def command_start(message: Message):
         caption=f'привет {message.from_user.full_name} для работы нажмите на кнопку',
         reply_markup=start_keyboard()
     )
+
+
+@router.message(F.text=='начать 🤠')
+async def handle_start_button(message: Message):
+    '''обработка кнопки начать'''
+
+    await handle_start(message)
+
+async def handle_start(message: Message):
+    await register_user(message)
+
+async def register_user(message: Message):
+    '''регистрация пользователя и доступ к основному меню'''
+    chat_id = message.chat.id
+    full_name = message.from_user.full_name
+
+    #todo реализовать логику с демонстрацией основного меню магазина
+    pass
