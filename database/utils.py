c38b3a23b7010fc2d8058925111d7c19e28ac760
paketkenchi@gmail.com <paketkenchi@gmail.com>
2026-07-20 20:29:35 +0300

    Создание кнопок и функций регистрации, получения номера телефона пользователя

---
+++ database/utils.py
@@ -0,0 +1,41 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+from database.base import engine
+from database.models import (Users,Products,Carts,Orders,Categories,FinallyCarts)
+from sqlalchemy import update, select
+
+def get_session():
+    return Session(engine)
+
+
+def db_register_user(full_name: str, chat_id: int):
+    """Регистрация юзера в базе"""
+
+    try:
+        with get_session() as session:
+            query = Users(name=full_name, telegram=chat_id)
+            session.add(query)
+            session.commit()
+        return False
+    except IntegrityError:
+        return True
+
+def db_update_user(chat_id: int, phone: str):
+    """получаем номер телефона польльзователя"""
+
+    with get_session() as session:
+        query = update(Users).where(Users.telegram == chat_id).values(phone=phone)
+        session.execute(query)
+        session.commit()
+
+def db_create_user_cart(chat_id: int):
+    '''создание корзины пользователя после регистрации'''
+    try:
+        with get_session() as session:
+            subquery = session.scalar(select(Users).where(Users.telegram == chat_id))
+            query = Carts(user_id=subquery.id)
+            session.add(query)
+            session.commit()
+            return True
+    except IntegrityError:
+        return False
--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,5 +1,5 @@
 from aiogram.types import KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardMarkup, ReplyKeyboardBuilder
 
 
 def start_keyboard():
@@ -10,3 +10,13 @@ def start_keyboard():
         ],
         resize_keyboard=True
     )
+
+def get_main_menu():
+    '''создание основного меню сервиса'''
+    builder = ReplyKeyboardMarkup()
+    builder.button(text='Оформить заказ 🚚')
+    builder.button(text='Корзина 🛒')
+    builder.button(text='История')
+    builder.button(text='Настройки ⚙')
+    builder.adjust(1,3)
+    return builder.as_markup(resize_keyboard=True)
